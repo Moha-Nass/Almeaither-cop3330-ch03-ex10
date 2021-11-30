bb499c87185426af6a5d6a68a4a97a8eba545456
Mohammed Almeaither <90289277+Moha-Nass@users.noreply.github.com>
2021-11-29 23:20:02 -0500

    Moha

---
+++ Test.cpp
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string operation;
+    double a,b, result;
+    cout<<"Enter operation and two operands: ";
+    cin>>operation>>a>>b;
+    if(operation=="+")
+    {
+        result = a+b;
+        cout<<a<<" + "<<b <<" = "<<result;
+    }
+    else if(operation=="-")
+    {
+        result = a-b;
+        cout<<a<<" - "<<b <<" = "<<result;
+    }
+    else if(operation=="/")
+    {
+        result = a/b;
+        cout<<a<<" / "<<b <<" = "<<result;
+    }
+    else if(operation=="*")
+    {
+        result = a*b;
+        cout<<a<<" * "<<b <<" = "<<result;
+    }
+    else
+    {
+        cout<<"invalid input"<<endl;
+    }
+    return 0;
+}
